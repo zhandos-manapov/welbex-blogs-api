a17d4fd2692f76d62aeeca0fb6a8dc3371c2c97d
zhandos-manapov <zandosmanapov@gmail.com>
2023-05-18 00:01:20 +0600

    try again

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -3,10 +3,11 @@ import express from 'express'
 // Routes
 import authRouter from './auth.router'
 import postRouter from './post.router'
+import authorize from '../middleware/auth.middleware'
 
 const index = express.Router()
 
 index.use('/api/v1/auth', authRouter)
-index.use('/api/v1/post', postRouter)
+index.use('/api/v1/post', authorize, postRouter)
 
 export default index
