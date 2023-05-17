6211f20c2b7faffb3447c6c322618f1bdc8f9676
zhandos-manapov <zandosmanapov@gmail.com>
2023-05-17 15:43:02 +0600

    try again

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -3,7 +3,7 @@ import { signin, signup } from '../controllers/auth.controller'
 
 const router = express.Router()
 
-router.post('/signup', signup)
-router.post('/signin', signin)
+router.route('/signin').post(signin)
+router.route('/signup').post(signup)
 
 export default router
